318ea3558c5d9a0096b735c645644ff2539c7549
jlayog <jerel.layog@gmail.com>
2022-06-21 09:54:41 -0400

    Update

---
+++ src/components/FormPersonalDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormPersonalDetails = () => {
+  return (
+    <div>FormPersonalDetails</div>
+  )
+}
+
+export default FormPersonalDetails;
---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/system';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  }
+});
+
+// Let's destructure the state passed to this component
+const FormUserDetails = (props) => {
+  const handleContinue = (e) => {
+    e.preventDefault();
+    props.nextStep();
+  }
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <AppBar title="Enter User Details"/>
+        <TextField
+          helperText="Enter Your First Name"
+          label="First Name"
+          onChange={props.handleChange('firstName')}
+          defaultValue={props.allInputs}
+        />
+        <br/>
+        <TextField
+          helperText="Enter Your Last Name"
+          label="First Name"
+          onChange={props.handleChange('firstName')}
+          defaultValue={props.allInputs}
+        />
+        <br/>
+        <TextField
+          helperText="Enter Your First Name"
+          label="First Name"
+          onChange={props.handleChange('firstName')}
+          defaultValue={props.allInputs}
+        />
+      </React.Fragment>
+    </ThemeProvider>
+  )
+}
+
+export default FormUserDetails;
